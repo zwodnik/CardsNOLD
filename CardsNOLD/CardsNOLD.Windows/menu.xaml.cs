7e1e7dcce12099bf325450365ad509976b7cab19
Jechbeat <jechbeatdev@gmail.com>
2014-12-13 05:44:24 +0100

    ZMIANY

--- CardsNOLD/CardsNOLD.Windows/menu.xaml.cs
+++ CardsNOLD/CardsNOLD.Windows/menu.xaml.cs
@@ -13,18 +13,28 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
 
 namespace CardsNOLD
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class BlankPage1 : Page
+    public sealed partial class menu : Page
     {
-        public BlankPage1()
+        public menu()
         {
             this.InitializeComponent();
+            menu_anim.Begin();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
         }
     }
 }
