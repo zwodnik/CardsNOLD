478027605808e46025fab9395d6d0336939b4c1e
GTom93 <gradzinskitomasz@gmail.com>
2014-12-14 05:56:21 +0100

    gui3

--- CardsNOLD/CardsNOLD.Windows/MainPage.xaml.cs
+++ CardsNOLD/CardsNOLD.Windows/MainPage.xaml.cs
@@ -14,7 +14,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
-
+//
 namespace CardsNOLD
 {
     /// <summary>
